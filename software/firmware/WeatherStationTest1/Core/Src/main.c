39e7b13035ae0d5f408559c8ec5fe68b1a8c8f0d
Rutland Harris <walter.r.harris07@gmail.com>
2026-08-03 16:44:25 -0500

    Complete I2C bring-up and verify BH1750 communication

--- software/firmware/WeatherStationTest1/Core/Src/main.c
+++ software/firmware/WeatherStationTest1/Core/Src/main.c
@@ -41,6 +41,8 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
+I2C_HandleTypeDef hi2c1;
+
 UART_HandleTypeDef huart2;
 
 /* USER CODE BEGIN PV */
@@ -51,6 +53,7 @@ UART_HandleTypeDef huart2;
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_USART2_UART_Init(void);
+static void MX_I2C1_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -90,6 +93,7 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_USART2_UART_Init();
+  MX_I2C1_Init();
   /* USER CODE BEGIN 2 */
 char buffer[64];
 uint32_t counter = 0;
@@ -100,19 +104,26 @@ uint32_t counter = 0;
   while (1)
   {
     /* USER CODE END WHILE */
-	  sprintf(buffer,
-	              "Portable Weather Station\r\n"
-	              "Counter = %lu\r\n\r\n",
-	              counter++);
+	  sprintf(buffer, "\r\nScanning I2C bus...\r\n");
+	  HAL_UART_Transmit(&huart2, (uint8_t *)buffer, strlen(buffer), HAL_MAX_DELAY);
 
-	      HAL_UART_Transmit(
-	          &huart2,
-	          (uint8_t *)buffer,
-	          strlen(buffer),
-	          HAL_MAX_DELAY);
+	  for (uint8_t address = 8; address < 120; address++)
+	  {
+	      if (HAL_I2C_IsDeviceReady(&hi2c1, address << 1, 2, 10) == HAL_OK)
+	      {
+	          sprintf(buffer, "Found device at 0x%02X\r\n", address);
+	          HAL_UART_Transmit(&huart2,
+	                            (uint8_t *)buffer,
+	                            strlen(buffer),
+	                            HAL_MAX_DELAY);
+	      }
+	  }
 
-	      HAL_Delay(1000);
-    /* USER CODE BEGIN 3 */
+	  sprintf(buffer, "Scan complete.\r\n\r\n");
+	  HAL_UART_Transmit(&huart2, (uint8_t *)buffer, strlen(buffer), HAL_MAX_DELAY);
+
+	  HAL_Delay(5000);
+	     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
 }
@@ -161,6 +172,40 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
+}
+
+/**
+  * @brief I2C1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_I2C1_Init(void)
+{
+
+  /* USER CODE BEGIN I2C1_Init 0 */
+
+  /* USER CODE END I2C1_Init 0 */
+
+  /* USER CODE BEGIN I2C1_Init 1 */
+
+  /* USER CODE END I2C1_Init 1 */
+  hi2c1.Instance = I2C1;
+  hi2c1.Init.ClockSpeed = 100000;
+  hi2c1.Init.DutyCycle = I2C_DUTYCYCLE_2;
+  hi2c1.Init.OwnAddress1 = 0;
+  hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+  hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+  hi2c1.Init.OwnAddress2 = 0;
+  hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+  hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  if (HAL_I2C_Init(&hi2c1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN I2C1_Init 2 */
+
+  /* USER CODE END I2C1_Init 2 */
+
 }
 
 /**
